0adfbd1022dd4dd982a43778e02d48ad84cf5f4b
NivPorat <73551160+NivPorat@users.noreply.github.com>
2021-04-03 16:10:13 +0300

    Add files via upload

---
+++ scripts/racket.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class racket : MonoBehaviour
+{
+    public float speed = 150;
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
